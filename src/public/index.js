d43a009218670dd867d28d48ecb3aa0f6d7ce88d
Ruchin Munjal <emailruchin@gmail.com>
2024-05-12 08:49:44 +0100

    formatting and HTML 5 compliant template used

--- src/public/index.js
+++ src/public/index.js
@@ -1,28 +1,40 @@
 // MDN WebSocket documentation
 // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
 
-const socket = new WebSocket('wss://qsr2p7233l.execute-api.eu-west-2.amazonaws.com/production/')
+const socket = new WebSocket(
+  "wss://qsr2p7233l.execute-api.eu-west-2.amazonaws.com/production/"
+);
 
-socket.addEventListener('open', e => {
-  console.log('WebSocket is connected')
-})
+socket.addEventListener("open", (e) => {
+  console.log("WebSocket is connected");
+});
 
-socket.addEventListener('close', e => console.log('WebSocket is closed'))
+socket.addEventListener("close", (e) => console.log("WebSocket is closed"));
 
-socket.addEventListener('error', e => console.error('WebSocket is in error', e))
+socket.addEventListener("error", (e) =>
+  console.error("WebSocket is in error", e)
+);
 
-socket.addEventListener('message', e => {
+socket.addEventListener("data", (e) => {
   // console.log('WebSocket received a message:', e)
-  console.log('Your answer is:', JSON.parse(e.data).message)
-})
+  console.log("Your answer is:", JSON.parse(e.data).message);
+});
 
-document.querySelector('button.primary')[0].addEventListener('click',e=>{
-  window.ask('Get data for the filter');
-})
+//select the button and attach an event listener
+//when the button is clicked, call the ask function with the message "get filtered data"
+//ask function sends the message to the server
+//the server sends the message back to the client
+//the client logs the message
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  console.log("Button clicked");
+  ask("get filtered data");
+});
+
 window.ask = function (msg) {
   const payload = {
-    action: 'data',
-    msg
-  }
-  socket.send(JSON.stringify(payload))
-}
+    action: "data",
+    msg,
+  };
+  socket.send(JSON.stringify(payload));
+};
